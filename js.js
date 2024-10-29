7e6e1b139653e4901e5ef6890362206f74c64ec7
Frcsora <150892201+Frcsora@users.noreply.github.com>
2024-10-29 20:51:35 +0100

    Update js.js

--- js.js
+++ js.js
@@ -1,36 +1,61 @@
-let fecha = new Date(Date.now());
-let fechaFormateada = fecha.toLocaleString();
-const fechaConcierto = new Date(2024, 9, 27, 19, 30)
-const [dias, horas] = timestamp(fechaConcierto)
-console.log(dias < 0 ?  "No puedes poner una fecha anterior" : "Queda" + (dias === 1  ? " " +  dias + " dia" :
-        dias > 1 ? "n " + dias + " días" :
-            dias === 0 && horas > 1 ? "n" : "") +
-    (dias > 0 ? " y " : "") +
-    ( horas === 1 ? " " + +horas + " hora" :
-        horas > 1 ? " " + horas + " horas" : ""))
+const fecha = new Date(Date.now());
+//const fechaFormateada = fecha.toLocaleString();
+const fechaConcierto = new Date(2024, 11, 22, 14, 30)
+const fechaConciertoFormateada = fechaConcierto.toLocaleString();
+const [dias, horas] = calculoDias();
 
-setInterval(function(){
+
+function calculoHoras(){
+    return Math.ceil((fechaConcierto.getTime() - fecha.getTime())
+    /(1000 * 60 * 60));
+}
+function calculoDias(){
+    const totalHoras = calculoHoras();
+    const dias = parseInt(totalHoras / 24);
+    const horas = totalHoras % 24;
+    return [dias, horas];
+}
+function determinarTemporada(fecha){
+    const temporada = ["Invierno", "Primavera", "Verano", "Otoño"];
+    const mes = fecha.getMonth();
+    let index = parseInt(mes / 3);
+    if(fecha.getMonth() % 3 === 2 && fecha.getDate() >= 25){
+        index++;
+    }
+    if(index >= 4){
+        index = 0;
+    }
+    return temporada[index];
+}
+function programarRecordatorio(diasAntes){
+    if(dias < diasAntes){
+        console.log(dias < 0 ?  "No puedes poner una fecha anterior"
+            : "Queda" + (dias === 1  ? " " +  dias + " dia" :
+                dias > 1 ? "n " + dias + " días" :
+                    dias === 0 && horas > 1 ? "n" : "") +
+            (dias > 0 ? " y" : "") +
+            (horas === 1 ? " " + +horas + " hora" :
+                horas > 1 ? " " + horas + " horas" : ""))
+        console.log("El concierto es el " + fechaConciertoFormateada + " en la estación de " + saberTemporada(fechaConcierto))
+    }
+}
+programarRecordatorio(54)
+/*setInterval(function(){
     fecha = new Date(Date.now())
     fechaFormateada = fecha.toLocaleString()
-    [dias, horas] = timestamp(fechaConcierto)
+        [dias, horas] = timestamp(fechaConcierto)
     console.log(dias < 0 ?  "No puedes poner una fecha anterior" : "Queda" + (dias === 1  ? " " +  dias + " dia" :
             dias > 1 ? "n " + dias + " días" :
                 dias === 0 && horas > 1 ? "n" : "") +
         (dias > 0 ? " y " : "") +
-        ( horas === 1 ? " " + +horas + " hora" :
-                horas > 1 ? " " + horas + " horas" : ""))
+        ( horas === 1 ? " " + + horas + " hora" :
+            horas > 1 ? " " + horas + " horas" : ""))
 
 }, 1000 * 60 * 60 * 24)
-
-function timestamp(fecha_concierto){
-    const dias = Math.floor((fecha_concierto.getTime() - fecha.getTime())
+*/
+/*function timestamp(fecha_concierto){
+    const dias = Math.ceil((fecha_concierto.getTime() - fecha.getTime())
         / (1000 * 60 * 60 * 24))
     const horas = Math.abs(fecha_concierto.getHours() - fecha.getHours())
-    return [dias, horas]
-}
-/*if(dias === 0 && fechaConcierto[1] > 0){
-        console.log(fechaFormateada + "\n" + (horas === 1 ? + horas + " hora para el inicio del evento" :
-            horas > 1 ? + horas + " horas para el inicio del evento" : ""))
-    }else if(dias === 0 && fechaConcierto[1] === 0){
-        console.log("El evento esta empezando!")
-    }*/
+    return [dias, horas];
+}*/
