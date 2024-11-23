dbaa57b53478c9cdd92f2bc7a89835011adb33df
Frcsora <150892201+Frcsora@users.noreply.github.com>
2024-11-23 15:09:39 +0000

    Add files via upload

--- tetris/tetris.js
+++ tetris/tetris.js
@@ -5,7 +5,22 @@ class Pieza{
         this.probabilidad = probabilidad;
         this.color = color;
     }
+    girarPieza() {
+        let nuevaForma = [];
+        for(let i = this.forma[0].length - 1 ; i >= 0 ; i--){
+            let nuevaFila = [];
+            for(let j = 0 ; j < this.forma.length ; j++){
+                nuevaFila.push(this.forma[j][i]);
+            }
+            nuevaForma.push(nuevaFila);
+        }
+        
+        this.forma = nuevaForma
+        
+        
+    }
 }
+
 const piezas = [
     new Pieza("C", [[1,1,1], [1,0,1]], 0.2, "red"),
     new Pieza("S", [[1,1], [1,1]], 0.3, "blue"),//s de square
@@ -13,6 +28,7 @@ const piezas = [
     new Pieza("T", [[1,1,1], [0,1,0], [0,1,0]], 0.2, "yellow"),
     new Pieza("Z", [[1,1,0], [0,1,1]], 0.2, "purple")
 ]
+
 const canvas = document.getElementById("tetris");
 const lienzo = canvas.getContext("2d");
 
@@ -20,10 +36,13 @@ const filas = 20;//height canvas = 600, 20 filas /600 px = 30px por fila(tamaño
 const columnas = 10;//width canvas = 300, 10 columnas / 300 px = 30px por columna(tamañoCelda)
 const tamañoCelda = 30;//tamaño en pixeles de la pieza
 
+
 let tablero = inicializarTablero();//el tablero se inicializa con todo 0
 let siguientePieza = generarPieza();//se elige la siguiente pieza que va a salir
-let avancePieza = 1;//El patron de inicializacion y posiciones que deben avanzar las piezas a cada iteracion
-let horizontalPieza = parseInt(tablero[0].length / 2);//La posicion inicial de las piezas en el eje Y
+let x = 1;//El patron de inicializacion y posiciones que deben avanzar las piezas a cada iteracion
+let y = parseInt(tablero[0].length / 2);//La posicion inicial de las piezas en el eje Y
+
+lienzo.strokeStyle = "white";
 function generarPieza(){
     //Elige cual sera la siguiente pieza que sale, tiene en cuenta la probabilidad de que salga cada pieza
     const resultado = Math.random()
@@ -42,29 +61,63 @@ function generarPieza(){
     }
 }
 
-setInterval(() =>{
+let juego = setInterval(() =>{
     //Todo el turno de caida de una pieza
-
-    dibujarTablero();
-    dibujoPieza(avancePieza - siguientePieza.forma.length, horizontalPieza -(parseInt(siguientePieza.forma.length / 2)), siguientePieza);
-    if(chequearColisiones(siguientePieza, avancePieza, horizontalPieza)){
-        insertarPieza(siguientePieza, avancePieza - siguientePieza.forma.length, horizontalPieza)
-        avancePieza = 0;
-        siguientePieza = generarPieza();
-    }
-    avancePieza++;
+    jugar();
     
 }, 500)
+function jugar(){
+    dibujarTablero();
+    actualizar();
+}
+function actualizar(){
+    if(y + siguientePieza.forma[0].length > 19){
+        y--;
+    }
+    dibujoPieza(siguientePieza, x - siguientePieza.forma.length, y -(parseInt(siguientePieza.forma.length / 2)));
+    
+    if(chequearColisiones(siguientePieza, x, y)){
+        insertarPieza(siguientePieza, x - siguientePieza.forma.length, y)
+        x = 0;
+        siguientePieza = generarPieza();
+    }
+    
+    x++;
+}
 function insertarPieza(pieza, x, y){
-    for(let i = 0 ; i < pieza.forma.length ; i++){
-        for(let j = 0 ; j < pieza.forma[i].length ; j++){
-            if(pieza.forma[i][j] == 1){
-                console.log("y: ", y, "j: ", j)
-                tablero[x + i][y + j - 1] = 1;
+    try {
+        for (let i = 0; i < pieza.forma.length; i++) {
+            for (let j = 0; j < pieza.forma[i].length; j++) {
+                if (tablero[x + i] === undefined || tablero[x + i][y + j - 1] === undefined) {
+                    throw new Error("Se acabó la partida");
+                }
+                if (pieza.forma[i][j] == 1) {
+                    tablero[x + i][y + j - 1] = 1;
+                }
+            }
+        }
+        eliminarLinea
+    } catch (error) {
+        clearInterval(juego);
+        document.getElementById("mensaje-final").innerText = error.message; 
+        document.getElementById("mensaje-final").style.backgroundColor = "yellow"; 
+        document.getElementById("mensaje-final").style.textAlign = "center"; 
+    }
+    
+}
+function eliminarLinea(){
+    for(let i = 0 ; i < tablero.length ; i++){
+        for(let j = 0 ; j < tablero[i].length ; j++){
+            if(tablero[i][j] != 1){
+                break;
+            }
+            if(j >= tablero[i].length - 1){
+                tablero.splice(i, 1)
+                nuevaFila = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+                tablero.unshift(nuevaFila)
             }
         }
     }
-    //Problema con las L, la siguiente pieza se come todo menos la fila de abajo de la l
 }
 function dibujarTablero(){
     //reinicia el tablero poniendo todo lo que no sean fichas anteriormente ya caidas como espacios vacios, para permitir dibujar el siguiente intervalo sin que se acumule con el anterior
@@ -73,40 +126,55 @@ function dibujarTablero(){
             if(tablero[i][j] != 1){
                 lienzo.fillStyle = "black";
                 lienzo.fillRect(j * tamañoCelda, i * tamañoCelda, tamañoCelda, tamañoCelda);
-                tablero[i][j] = 0;
+                lienzo.strokeRect(j * tamañoCelda, i * tamañoCelda, tamañoCelda, tamañoCelda)
             } else{
                 lienzo.fillStyle = "grey"
                 lienzo.fillRect(j * tamañoCelda, i * tamañoCelda, tamañoCelda, tamañoCelda);
+                lienzo.strokeRect(j * tamañoCelda, i * tamañoCelda, tamañoCelda, tamañoCelda    )
             }
             
         }
     }
-    
-
 }
 
-function dibujoPieza(x, y, pieza){
+function dibujoPieza(pieza, x, y){
     //dibuja la situacion en la presente iteracion del intervalo
+    
     for(let i = pieza.forma.length - 1 ; i >= 0 ; i--){
         if(i < 0) continue;
         for(let j = 0 ; j < pieza.forma[i].length ; j++){
             if(pieza.forma[i][j] == 1){                
                 lienzo.fillStyle = pieza.color;
                 lienzo.fillRect((j + y) * tamañoCelda, (i + x)* tamañoCelda, tamañoCelda, tamañoCelda);
+                lienzo.strokeRect((j + y) * tamañoCelda, (i + x)* tamañoCelda, tamañoCelda, tamañoCelda)
             }
         }
     }
 }
 
 function chequearColisiones(pieza, x, y){
-    for(let i = 0 ; i < pieza.forma[0].length;i++){
-        if(x >= filas || tablero[x][y + i] == 1) return true
+    for(let i = 0 ; i < pieza.forma.length;i++){
+        console.log(x)
+        for(let j = 0 ; j < pieza.forma[i].length ; j++){
+            
+            if( x >= filas ) return true;
+            if(pieza.forma[i][j] == 1){
+                if(tablero[i + x][j + y] == 1) return true
+            }
+        }
     }
-
-    
     return false;
 }
-
+function mostrarTablero(){
+    let str = "";
+    for(let i = 0 ; i < tablero.length ;i++){
+        for(let j = 0 ; j < tablero[i].length ; j++){
+            str += tablero[i][j];
+        }
+        str += "\n";
+    }
+    console.log(str)
+}
 function inicializarTablero(){
     //devuelve un array con las dimensiones que queremos lleno de 0's
     let array = [];
@@ -119,3 +187,24 @@ function inicializarTablero(){
     }
     return array;
 }
+
+document.addEventListener("keypress", (event) => {
+
+    if((event.key == "a" || event.key == "A") 
+        && y >= 2 && 
+        (tablero[x][y - 1] !== undefined && tablero[x][y - 1] !== 1)){
+        y--;
+    }
+    if((event.key == "d" || event.key == "D") && y <= tablero[0].length - siguientePieza.forma[0].length){
+        y++;
+    }
+    if(event.key == "s" ||event.key == "S" && x < filas){
+        if(x >= 20) x = 19
+        if(!chequearColisiones(siguientePieza, x, y)){
+            x++;    
+        }        
+    }
+    if(event.key == "w" || event.key == "W"){
+        siguientePieza.girarPieza();
+    }
+})
